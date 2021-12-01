46a00d1b2ea9aaf8e19b30ef14c94eeb7beec59e
Nick Gerow <nick.g.123@hotmail.com>
2021-12-01 08:09:05 -0500

    Added day 1

---
+++ day1/day1.py
@@ -0,0 +1,23 @@
+"""Day 1."""
+
+from itertools import islice, pairwise, tee
+
+from file_parser import Parser
+from result import Result
+
+
+def run(parser: Parser) -> Result:
+    """Solution for Day 1."""
+    # Use pairwise to count the number of increases
+    part1 = sum(b > a for a, b in pairwise(parser.read_ints()))
+
+    # Basically do the same thing, except increase the end iterator by 3 instead of just 1
+    window_start, window_end = tee(parser.read_ints())
+    window_end = islice(window_end, 3, None)
+
+    part2 = sum(
+        num_adding > num_leaving
+        for num_adding, num_leaving in zip(window_end, window_start)
+    )
+
+    return Result(part1, part2)
